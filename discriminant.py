c35d70bf2096a8e58df1a7d9fda5504497846b17
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2020-11-29 20:17:20 +0530

    Add files via upload

---
+++ discriminant.py
@@ -0,0 +1,14 @@
+#program to calculate discriminant value
+a=float(input("Enter the first value point a: "))
+b=float(input("Enter the second value point b: "))
+c=float(input("Enter the third value point c: "))
+#calculate discriminant value
+dis=(b**2)-4*(a*c)
+#to display
+print("discriminant value is",dis)
+
+#output
+Enter the first value point a: 4
+Enter the second value point b: 8
+Enter the third value point c: 2
+discriminant value is 32.0
---
+++ rad_deg.py
@@ -0,0 +1,9 @@
+#python program to convert radian to degree
+import math
+rad=float(input("enter the radians: "))#to input radian
+degree=rad*(180/math.pi)# equation to converting radian to degree
+print("%s Degree"%(degree))
+
+#output
+enter the radians: 5.2
+297.9380534680281 Degree
