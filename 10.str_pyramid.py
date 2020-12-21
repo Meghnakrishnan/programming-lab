8fbf844556b218a2a21e9f9c850e015022a646e5
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2020-12-21 22:22:36 +0530

    Add files via upload

---
+++ 10.str_pyramid.py
@@ -0,0 +1,34 @@
+'''Construct following pattern using nested loop
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*  '''
+
+row = 5
+for i in range(0, row):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print()
+
+for i in range(row, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print()
+
+
+'''output
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* '''
