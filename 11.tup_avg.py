e0ff2c356376335e53cf0cf0fde5a5b3e6aba8f7
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2020-12-28 18:39:51 +0530

    Add files via upload

---
+++ 11.tup_avg.py
@@ -0,0 +1,10 @@
+'''Write a Python program to calculate the average value of
+the numbers in a given tuple.'''
+
+
+tuple1=(2,3,7)
+avg=sum(tuple1)/len(tuple1)#calculate average
+print("Average:",avg)#display average
+
+#output
+Average: 4.0
