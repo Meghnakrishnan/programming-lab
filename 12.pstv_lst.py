3ea2a0332920ce0cc8233f0c4dd7b5927cbc7629
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2020-12-29 11:34:56 +0530

    Add files via upload

---
+++ 12.pstv_lst.py
@@ -0,0 +1,8 @@
+#Generate positive list of numbers from a given list of integers
+
+l=[-1,3,6]
+pstv=[x for x in l if x>0] #check numbers in given list & positive numbers store in  variable pstv
+print(pstv) #print positive list
+
+#output
+[3, 6]
