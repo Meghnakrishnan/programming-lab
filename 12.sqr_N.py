7655b4f25fea85b28e44cd8e133b9b2fac2e9147
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2021-01-02 16:40:16 +0530

    Add files via upload

---
+++ 12.sqr_N.py
@@ -0,0 +1,14 @@
+#Square of N numbers
+list=[1,2,4,8,9]
+for i in list: #finding square of each numbers in given list
+    sqr=i**2
+    print(sqr) #display the squares of numbrs
+
+    
+
+ #output
+1
+4
+16
+64
+81
