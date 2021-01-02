bf1d4e97baac89f64500df0fe018ee9ca2dfa035
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2021-01-02 21:51:02 +0530

    Add files via upload

---
+++ 13.dict_merge.py
@@ -0,0 +1,9 @@
+#Merge two dictionaries.
+
+dict1={'name':'jon','age':'26'}
+dict2={'gender':'male','weight':'60'}
+dict1.update(dict2)#merge of dictionary two to dictionary one
+print(dict1)# print merged dictionary
+
+#output
+{'name': 'jon', 'age': '26', 'gender': 'male', 'weight': '60'}
