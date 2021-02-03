5d2354e3c55b80e07b91747a573fd20395318c26
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2021-02-03 19:52:08 +0530

    Add files via upload

---
+++ 18-area_perimeter.py
@@ -0,0 +1,15 @@
+import rectangle
+import circle
+print('Area of rectangle:',rectangle.get_area(10,20))
+print('perimeter of rectangle:',rectangle.get_perimeter(10,20))
+
+print('Area of circle:',circle.get_area(5))
+print('perimeter of circle:',circle.get_perimeter(5))
+
+
+'''output
+Area of rectangle: 200
+perimeter of rectangle: 60
+Area of circle: 78.53981633974483
+perimeter of circle: 31.41592653589793
+'''
